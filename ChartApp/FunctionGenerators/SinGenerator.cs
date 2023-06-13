db505dce5274cc6f5ede35d9cdb43eab60432439
Михаил Зюляев <zyulyaev12@gmail.com>
2023-06-13 15:23:02 +0400

    Added tests

--- ChartApp/FunctionGenerators/SinGenerator.cs
+++ ChartApp/FunctionGenerators/SinGenerator.cs
@@ -13,7 +13,7 @@ namespace ChartApp.FunctionGenerators
         private double _samplingFrequency;              //Частота дискретизации (опроса)
         private double _phase;                          //Фаза синусойды
   
-        private double[] signal;                        //Массив со сгенерированными данными синусойды
+        private double[] _signal;                        //Массив с сгенерированными данными синусойды
 
         public SinGenerator(double amplitude, double frequency, double countSecondsInterval = 1, double samplingFrequency = 1000)
         {
@@ -24,7 +24,7 @@ namespace ChartApp.FunctionGenerators
             Interval = TimeSpan.FromSeconds(countSecondsInterval);
 
             var count = (int)(Interval.TotalSeconds * samplingFrequency);  //Расчёт количества элемента массива исходя из времени окна и частоты дискретизации
-            signal = new double[count];
+            _signal = new double[count];
         }
 
         /// <summary>
@@ -33,16 +33,16 @@ namespace ChartApp.FunctionGenerators
         /// <returns>Сгенерированный массив данных синусойды.</returns>
         public double[] Generate()
         {
-            for(int i = 0; i < signal.Length; i++)
+            for(int i = 0; i < _signal.Length; i++)
             {
-                signal[i] = _amplitude * Math.Sin(2 * Math.PI * _frequency * i / _samplingFrequency + _phase);      //Генерация значения элемента массива
+                _signal[i] = _amplitude * Math.Sin(2 * Math.PI * _frequency * i / _samplingFrequency + _phase);      //Генерация значения элемента массива
             }
 
             _phase += _frequency;                       //Изменение фазы для создания эффекта движущейся волны
             if (_phase > _samplingFrequency)            //Сброс фазы, если её значение больше частоты дискретизации - для предотвращения ошибки переполнения
                 _phase = 0.0;
 
-            return signal;
+            return _signal;
         }
     }
 }
---
+++ ChartAppTests/SinGeneratorTests.cs
@@ -0,0 +1,52 @@
+using ChartApp.FunctionGenerators;
+using System.Reflection;
+
+namespace ChartAppTests
+{
+    public class SinGeneratorTests
+    {
+        [Test]
+        public void TestConstructor()
+        {
+            var sinGenerator = new SinGenerator(10, 1, 2, 100);
+            Type typeGenerator = typeof(SinGenerator);
+
+            var amplitudeFieldInfo = typeGenerator.GetField("_amplitude", BindingFlags.Instance | BindingFlags.NonPublic);
+            var amplitudeValue = amplitudeFieldInfo?.GetValue(sinGenerator);
+
+            var frequencyFieldInfo = typeGenerator.GetField("_frequency", BindingFlags.Instance | BindingFlags.NonPublic);
+            var frequencyValue = frequencyFieldInfo?.GetValue(sinGenerator);
+
+            var samplingFrequencyFieldInfo = typeGenerator.GetField("_samplingFrequency", BindingFlags.Instance | BindingFlags.NonPublic);
+            var samplingFrequencyValue = samplingFrequencyFieldInfo?.GetValue(sinGenerator);
+
+            var phaseFieldInfo = typeGenerator.GetField("_phase", BindingFlags.Instance | BindingFlags.NonPublic);
+            var phaseValue = phaseFieldInfo?.GetValue(sinGenerator);
+
+            var signalFieldInfo = typeGenerator.GetField("_signal", BindingFlags.Instance | BindingFlags.NonPublic);
+            var signalValue = signalFieldInfo?.GetValue(sinGenerator);
+
+            Assert.That(amplitudeValue, Is.EqualTo(10));
+            Assert.That(frequencyValue, Is.EqualTo(1));
+            Assert.That(samplingFrequencyValue, Is.EqualTo(100));
+            Assert.That(phaseValue, Is.EqualTo(0.0));
+            Assert.That(signalValue, Has.Length.EqualTo(200));
+            Assert.That(sinGenerator.Interval, Is.EqualTo(new TimeSpan(0, 0, 2)));
+        }
+
+        [Test]
+        public void TestGenerator()
+        {
+            var sinGenerator = new SinGenerator(5, 10, 1, 10);
+            var resultArray = sinGenerator.Generate();
+
+            double[] expectedArray = new double[10];
+            for(int i = 0; i < expectedArray.Length; i++)
+            {
+                expectedArray[i] = 5 * Math.Sin(2 * Math.PI * 10 * i / 10);
+            }
+
+            Assert.That(resultArray, Is.EqualTo(expectedArray));
+        }
+    }
+}
